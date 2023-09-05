8cd35ca9a4f0baa3b0b04c0b350d314be5b2f0cd
Raman Sehgal <sc.ramansehgal@gmail.com>
2023-09-05 16:11:51 +0530

    Tried to add bootstrap.

--- Button.js
+++ Button.js
@@ -2,5 +2,5 @@
 function Button(prop){
     const {type,className,disabled,children}=prop;
     //return <button type={type} className={className} disabled={disabled}>{children}</button>;
-    return <button type={type} className="ui-button" disabled={disabled}>{children}</button>
+    return <button type={type} className={className} disabled={disabled}>{children}</button>
 }
